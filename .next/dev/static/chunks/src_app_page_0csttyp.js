e6303016b4ab1ee3fa68fd0b11fc3ecb973072d7
Anshuman Mishra <157224477+ansm32004@users.noreply.github.com>
2026-07-28 22:36:09 +0530

    xy

---
+++ .next/dev/static/chunks/src_app_page_0csttyp.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/src_components_1t1qr9d._.js","static/chunks/node_modules_19tcm82._.js"],
+    source: "dynamic"
+});
